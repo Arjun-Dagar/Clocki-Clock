c7ba86d823e1680a6f5d4faaa46b6139926c35d5
Arjun <arjundagar28@gmail.com>
2021-08-04 01:00:40 +0530

    Added event timer

--- action.js
+++ action.js
@@ -1,7 +1,6 @@
-window.onload = function(){
+window.onload = function () {
   document.getElementById("preloader").style.display = "none";
 };
-
 
 setInterval(() => {
   var d = new Date();
@@ -33,7 +32,15 @@ setInterval(() => {
     "December",
   ];
 
-  var dayname = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+  var dayname = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
 
   document.getElementById("hour").style.transform = `rotate(${h}deg)`;
   document.getElementById("minute").style.transform = `rotate(${m}deg)`;
@@ -43,3 +50,113 @@ setInterval(() => {
   document.getElementById("year").textContent = year;
   document.getElementById("day").textContent = dayname[day];
 }, 1000);
+
+var item;
+
+var check = false;
+
+function timer() {
+
+  if (!check) {
+    let date = document.getElementsByTagName("select")[0];
+    let month = document.getElementsByTagName("select")[1];
+    let year = document.getElementsByTagName("select")[2];
+    let hour = document.getElementsByTagName("select")[3];
+    let min = document.getElementsByTagName("select")[4];
+    let timerdate = new Date(
+      year.value,
+      month.value - 1,
+      date.value,
+      hour.value,
+      min.value
+    );
+
+    check = true;
+
+    item = setInterval(() => {
+      let currdate = new Date();
+
+      let os = Math.floor(((timerdate - currdate) / 1000) % 60);
+      let om = Math.floor(((timerdate - currdate) / (1000 * 60)) % 60);
+      let oh = Math.floor(((timerdate - currdate) / (1000 * 60 * 60)) % 24);
+      let od = Math.floor((timerdate - currdate) / (1000 * 60 * 60 * 24));
+
+      if (os < 0 || om < 0 || oh < 0 || od < 0) {
+        alert("Enter valid time");
+        clearInterval(item);
+        return;
+      }
+
+      document.querySelectorAll(".time p")[0].innerHTML = numeric(od);
+      document.querySelectorAll(".time p")[1].innerHTML = numeric(oh);
+      document.querySelectorAll(".time p")[2].innerHTML = numeric(om);
+      document.querySelectorAll(".time p")[3].innerHTML = numeric(os);
+    }, 1000);
+  } 
+
+  else {
+    clearInterval(item);
+    let date = document.getElementsByTagName("select")[0];
+    let month = document.getElementsByTagName("select")[1];
+    let year = document.getElementsByTagName("select")[2];
+    let hour = document.getElementsByTagName("select")[3];
+    let min = document.getElementsByTagName("select")[4];
+    let timerdate = new Date(
+      year.value,
+      month.value - 1,
+      date.value,
+      hour.value,
+      min.value
+    );
+
+    check = true;
+
+    item = setInterval(() => {
+      let currdate = new Date();
+
+      let os = Math.floor(((timerdate - currdate) / 1000) % 60);
+      let om = Math.floor(((timerdate - currdate) / (1000 * 60)) % 60);
+      let oh = Math.floor(((timerdate - currdate) / (1000 * 60 * 60)) % 24);
+      let od = Math.floor((timerdate - currdate) / (1000 * 60 * 60 * 24));
+
+      if (os == 0 && om == 0 && oh == 0 && od == 0) {
+        alert("Yeah!!, This is your event time");
+        document.querySelectorAll(".time p")[3].innerHTML = 00;
+        clearInterval(item);
+        return;
+      }
+
+      if (os < 0 || om < 0 || oh < 0 || od < 0) {
+        alert("Enter valid time");
+        clearInterval(item);
+        return;
+      }
+      
+
+      document.querySelectorAll(".time p")[0].innerHTML = numeric(od);
+      document.querySelectorAll(".time p")[1].innerHTML = numeric(oh);
+      document.querySelectorAll(".time p")[2].innerHTML = numeric(om);
+      document.querySelectorAll(".time p")[3].innerHTML = numeric(os);
+    }, 1000);
+  }
+}
+
+function numeric(key) {
+  return key < 10 ? `0${key}` : key;
+}
+
+
+document.getElementById("first-button").addEventListener("click", function () {  
+  // console.log("yes");
+  document.body.style.transform = "translateX(-100vw)";
+})
+
+document.getElementById("second-button").addEventListener("click", function () {  
+  // console.log("yes");
+  document.body.style.transform = "translateX(0vw)";
+})
+
+document.getElementById("third-button").addEventListener("click", function () {  
+  // console.log("yes");
+  document.body.style.transform = "translateX(-200vw)";
+})
